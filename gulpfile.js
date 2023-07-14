678a7193cd2eecb16c35c10153137f35b45334fb
Patrice Chalin <pchalin@gmail.com>
2023-07-14 07:08:56 -0400

    Improved gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,57 @@ const markdownlint = require("markdownlint");
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-const markdownFiles = ["**/*.md", "!**/node_modules/**", "!**/.github/**"];
-const config = yaml.load(fs.readFileSync("./.markdownlint.yaml", "utf8"));
-
-let fileCounter = 0;
+let fileCount = 0,
+  issueCount = 0;
 
 function markdownLintFile(file, encoding, callback) {
+  const config = yaml.load(fs.readFileSync("./.markdownlint.yaml", "utf8"));
   const options = {
     files: [file.path],
     config: config,
   };
 
   markdownlint(options, function (err, result) {
+    if (err) {
+      console.error("ERROR occurred while running markdownlint: ", err);
+      return callback(err);
+    }
+
     const _resultString = (result || "").toString();
-    // Result is of the form:
-    // "<file-path>:\s*<line-number>: <ID-and-message"
-    // Strip out the whitespace between the file-path and line number
+    // Result is a string with lines of the form:
+    //
+    //   <file-path>:\s*<line-number>: <ID-and-message>
+    //
+    // Strip out any whitespace between the filepath and line number
     // so that tools can jump directly to the line.
-    const resultString = _resultString.replace(
-      /([^:]+):\s*(\d+):(.*)/,
-      "$1:$2:$3",
-    );
-    if (resultString) console.log(resultString);
-    fileCounter++;
+    const resultString = _resultString
+      .split("\n")
+      .map((line) => line.replace(/^([^:]+):\s*(\d+):(.*)/, "$1:$2:$3"))
+      .join("\n");
+    if (resultString) {
+      console.log(resultString);
+      issueCount++;
+    }
+    fileCount++;
     callback(null, file);
   });
 }
 
 function lintMarkdown() {
-  return (
-    gulp
-      .src(markdownFiles)
-      .pipe(through2.obj(markdownLintFile))
-      .on("end", () => {
-        console.log(`Processed ${fileCounter} file${fileCounter == 1 ? '' : 's'}.`);
-      })
-  );
+  const markdownFiles = ["**/*.md", "!**/node_modules/**", "!**/.github/**"];
+
+  return gulp
+    .src(markdownFiles)
+    .pipe(through2.obj(markdownLintFile))
+    .on("end", () => {
+      console.log(
+        `Processed ${fileCount} file${
+          fileCount == 1 ? "" : "s"
+        }, ${issueCount} had issues.`,
+      );
+    });
 }
+
+lintMarkdown.description = `Run markdownlint on all '*.md' files.`;
 
 gulp.task("lint-md", lintMarkdown);
