634823f194ca7a3403dafa7aef208e2a6bc4f641
Joao Grassi <joao.grassi@dynatrace.com>
2023-11-23 14:48:19 +0100

    Update spec badge via script

--- internal/tools/update_specification_version.sh
+++ internal/tools/update_specification_version.sh
@@ -12,6 +12,8 @@ LATEST_SPECIFICATION_VERSION="v1.26.0"
 # The specific pattern we look for when replacing URLs
 SPECIFICATION_URL_PREFIX="https://github.com/open-telemetry/opentelemetry-specification/tree/"
 SPECIFICATION_BLOB_URL_PREFIX="https://github.com/open-telemetry/opentelemetry-specification/blob/"
+# The specific pattern we look for updating the Badge with the spec version
+SPECIFICATION_BADGE_PREFIX="https://img.shields.io/badge/Specification_version-"
 
 
 fix_file() {
@@ -19,6 +21,7 @@ fix_file() {
   sed -i \
    -e "s,${SPECIFICATION_URL_PREFIX}${PREVIOUS_SPECIFICATION_VERSION},${SPECIFICATION_URL_PREFIX}${LATEST_SPECIFICATION_VERSION},g" \
    -e "s,${SPECIFICATION_BLOB_URL_PREFIX}${PREVIOUS_SPECIFICATION_VERSION},${SPECIFICATION_URL_PREFIX}${LATEST_SPECIFICATION_VERSION},g" \
+   -e "s,${SPECIFICATION_BADGE_PREFIX}${PREVIOUS_SPECIFICATION_VERSION},${SPECIFICATION_BADGE_PREFIX}${LATEST_SPECIFICATION_VERSION},g" \
    "$1"
 }
 
