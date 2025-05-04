f16f5f1417ab9217af014be1700027dde6ff3f53
Nathan L Smith <nathan.smith@elastic.co>
2025-05-03 22:25:27 -0500

    add mermaid script; use resource instead of entity

---
+++ internal/tools/scripts/generate-mermaid.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+
+function extractEntitiesAndRelationships(baseDir) {
+  const entities = [];
+
+  function processFile(filePath) {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const data = yaml.load(fileContent);
+
+    if (data.groups) {
+      data.groups.forEach((group) => {
+        if (group.type === "resource") {
+          entities.push(group);
+        }
+      });
+    }
+  }
+
+  function walkDirectory(dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file);
+      if (fs.statSync(fullPath).isDirectory()) {
+        walkDirectory(fullPath);
+      } else if (file === "resources.yaml") {
+        processFile(fullPath);
+      }
+    });
+  }
+
+  walkDirectory(baseDir);
+  return entities;
+}
+
+function generateMermaidDiagram(entities) {
+  console.log("classDiagram");
+  for (const entity of entities) {
+    for (const attr of entity.attributes) {
+      // The word "namespace" is reserved and I don't know how to quote it
+      const name = entity.name.replace("namespace", "namespac3");
+      console.log(`  ${name} : attribute ${attr.ref}`);
+    }
+    for (const attr of entity.descriptive_attributes ?? []) {
+      console.log(`  ${entity.name} : descriptive_attribute ${attr.ref}`);
+    }
+    for (const relationship of entity.relationships ?? []) {
+      for ([relationshipType, relatedEntityType] of Object.entries(
+        relationship,
+      )) {
+        console.log(
+          `  ${entity.name} --> ${relatedEntityType}: ${relationshipType}`,
+        );
+      }
+    }
+  }
+}
+
+generateMermaidDiagram(
+  extractEntitiesAndRelationships(path.join(__dirname, "../../..", "model")),
+);
