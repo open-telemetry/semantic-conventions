c428fb723d52a25018ac42f18286360b7c3cbdb0
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2025-10-02 13:36:08 +0200

    Allow skipping check based on labels

--- .github/scripts/triage/check-changes-ownership.ts
+++ .github/scripts/triage/check-changes-ownership.ts
@@ -5,31 +5,48 @@ const octokit = new Octokit();
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY!.split("/");
 const prNumber: number = +process.env.PR_NUMBER!;
-const changes : string[] = process.env.CHANGED_FILES!.split(',');
+const changes: string[] = process.env.CHANGED_FILES!.split(',');
+
+/**
+ * Checks if the PR already has the 'triage:accepted:ready' label, meaning the triage checks should be skipped.
+ * @returns true if the PR has the 'triage:accepted:ready' assigned to it, false otherwise.
+ */
+async function shouldSkipCheck() {
+    const result = await octokit.request("GET /repos/{owner}/{repo}/issues/{issue_number}/labels", {
+        owner: owner,
+        repo: repo,
+        issue_number: prNumber
+    });
+
+    return result.data.some(l => l.name === "triage:accepted:ready");
+}
 
 function getCommentText(changesWithoutOwners: string[]): string {
     return `
-This PR contains changes to area(s) that do not have an active SIG/project:
+This PR contains changes to area(s) that do not have an active SIG/project and will be auto-closed:
 
 - ${changesWithoutOwners.join('\n- ')}
 
 Such changes may be rejected or put on hold until a new SIG/project is stablished.
 
-Please refer to the [Semantic Convention Areas](./https://github.com/open-telemetry/semantic-conventions/blob/main/AREAS.md)
-document to see the current active SIGs and also to learn how to kick start a new one.
-
-If there's no interaction to this PR, it will be auto-closed.`;
+Please refer to the [Semantic Convention Areas](https://github.com/open-telemetry/semantic-conventions/blob/main/AREAS.md)
+document to see the current active SIGs and also to learn how to kick start a new one.`;
 }
 
 async function changesInInactiveAreas(): Promise<boolean> {
-    const areas = utils.getAllAreasMetadata();
+    // skips enforcing the triage process if the PR has the 'triage:accepted:ready' label on it
+    // this means maintainers/approvers decided to bypass it for good reasons.
+    if (await shouldSkipCheck()) {
+        return false;
+    }
 
-    let changesWithoutOwners: string[] = [];
+    const areas = utils.getAllAreasMetadata();
     const areaOwnersMap = utils.getActiveAreasWithCodeOwners(areas);
 
     // extract only the name after model/ which is the actual area name
     const changedAreas = changes.map(folder => folder.split('/')[1]);
 
+    let changesWithoutOwners: string[] = [];
     changedAreas.forEach(ca => {
         if (!areaOwnersMap.has(ca)) {
             changesWithoutOwners.push(ca);
@@ -43,12 +60,18 @@ async function changesInInactiveAreas(): Promise<boolean> {
             issue_number: prNumber,
             labels: ['triage:rejected:declined']
         });
-
         await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
             owner: owner,
             repo: repo,
             issue_number: prNumber,
             body: getCommentText(changesWithoutOwners)
+        });
+        await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+            owner: owner,
+            repo: repo,
+            issue_number: prNumber,
+            state: 'closed',
+            state_reason: 'not_planned'
         });
 
         return true
